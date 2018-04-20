de8d84bc105d6ae81100743bc7dcd062ea79fe51
mrw008 <33675115+mrw008@users.noreply.github.com>
2018-04-20 11:47:51 -0700

    Update BBB_32_mux_switching.c

--- BBB_c_code/BBB_32_mux_switching.c
+++ BBB_c_code/BBB_32_mux_switching.c
@@ -9,8 +9,13 @@ static int set_gpio_dir(int pin, int dir);
 static int set_gpio_value(int pin, int val);
 static int unexport_gpio(int pin);
 static int read_gpio(int pin);
+static int adc_init(int channel);
+static int adc_cleanup(int channel);
 static int read_adc_raw(int channel);
 static int clean_up(int pin);
+
+int fd_adc[4];
+int volt_channel = 0;
 
 int main(){
 
@@ -94,7 +99,7 @@ int main(){
         set_gpio_value(mux_a3, chan[mux[i][j]-1][1]);
         set_gpio_value(mux_a4, chan[mux[i][j]-1][0]);
 
-        int volt_channel = 0;
+        
         int value = read_adc_raw(volt_channel);
         float voltage = value*bits_to_volts;
         printf(" %.3f",voltage);
@@ -125,24 +130,31 @@ int main(){
   return 0;
 }
 
-static int read_adc_raw(int channel){
+static int adc_init(int channel){
   char path[66];
-  char value_read[20];
-  int fd;
+  int temp_fd;
   snprintf(path,66,"/sys/bus/iio/devices/iio:device1/in_voltage%d_raw",channel);
-  fd = open(path,O_WRONLY);
+  temp_fd = open(path,O_RDONLY);
   if (-1 == fd){
     fprintf(stderr,"Failed to open adc for reading!\n");
     return -1;
   }
-  int fd_read = read(fd,value_read,20);
+  fd_adc[channel] = temp_fd;
+}
+
+static int read_adc_raw(int channel){
+  char value_read[20];
+  int fd_read = read(fd_adc[channel],value_read,20);
   if (-1 == fd_read)) {
     fprintf(stderr, "Failed to read value!\n");
     return(-1);
   }
-  close(fd);
   return(atoi(value_read));
+}
 
+static int adc_cleanup(int channel){
+  close(fd_adc[channel]);
+  return 0;
 }
 
 
