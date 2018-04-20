51ba8236524e21e94f66a841fea162f7bb38ebe2
mrw008 <33675115+mrw008@users.noreply.github.com>
2018-04-19 18:43:57 -0700

    Update and rename BBB_test_code.c to BBB_32_mux_switching.c

--- BBB_c_code/BBB_32_mux_switching.c
+++ BBB_c_code/BBB_32_mux_switching.c
@@ -9,9 +9,11 @@ static int set_gpio_dir(int pin, int dir);
 static int set_gpio_value(int pin, int val);
 static int unexport_gpio(int pin);
 static int read_gpio(int pin);
+static int read_adc_raw(int channel);
+static int clean_up(int pin);
 
 int main(){
-/*
+
   char chan[32][5] = {{0,0,0,0,0},{0,0,0,0,1},{0,0,0,1,0},{0,0,0,1,1},
                       {0,0,1,0,0},{0,0,1,0,1},{0,0,1,1,0},{0,0,1,1,1},
                       {0,1,0,0,0},{0,1,0,0,1},{0,1,0,1,0},{0,1,0,1,1},
@@ -27,20 +29,120 @@ int main(){
   for(int i = 0; i<=31,i++){
     for(int j= 0;j<=31;j++){
       if(demux1[i] != demux1[j] && demux2[i] != demux1[j]){
-        mux[i][j] = demux1[j];
+        mux[i][k] = demux1[j];
         k++;
       }
     }
     k = 0;
   }
-  */
-  int pin = 57;
-  int dir = 1;
-  int value = 1;
-  export_gpio(pin);
-  set_gpio_dir(pin, dir);
-  set_gpio_value(pin,value)
+  int demux1_a0 = 24;
+  int demux1_a1 = 26;
+  int demux1_a2 = 28;
+  int demux1_a3 = 30;
+  int demux1_a4 = 32;
+
+  int demux2_a0 = 34;
+  int demux2_a1 = 36;
+  int demux2_a2 = 38;
+  int demux2_a3 = 40;
+  int demux2_a4 = 42;
+
+  int mux_a0 = 44;
+  int mux_a1 = 46;
+  int mux_a2 = 48;
+  int mux_a3 = 50;
+  int mux_a4 = 52;
+//setting the direction of gpio pins used for muxes to outputs(1)
+  set_gpio_dir(demux1_a0,1);
+  set_gpio_dir(demux1_a1,1);
+  set_gpio_dir(demux1_a2,1);
+  set_gpio_dir(demux1_a3,1);
+  set_gpio_dir(demux1_a4,1);
+  set_gpio_dir(demux2_a0,1);
+  set_gpio_dir(demux2_a1,1);
+  set_gpio_dir(demux2_a2,1);
+  set_gpio_dir(demux2_a3,1);
+  set_gpio_dir(demux2_a4,1);
+  set_gpio_dir(mux_a0,1);
+  set_gpio_dir(mux_a1,1);
+  set_gpio_dir(mux_a2,1);
+  set_gpio_dir(mux_a3,1);
+  set_gpio_dir(mux_a4,1);
+
+  int bits_to_volts = 5/32767;
+  int flag = 0;
+  while(flag < 200){
+    for(int i = 0; i<=31; i++){
+      //power and ground distribution
+      set_gpio_value(demux1_a0,chan[demux1[i]-1][4]);
+      set_gpio_value(demux1_a1,chan[demux1[i]-1][3]);
+      set_gpio_value(demux1_a2,chan[demux1[i]-1][2]);
+      set_gpio_value(demux1_a3,chan[demux1[i]-1][1]);
+      set_gpio_value(demux1_a4,chan[demux1[i]-1][0]);
+
+      set_gpio_value(demux2_a0,chan[demux1[i]-1][4]);
+      set_gpio_value(demux2_a1,chan[demux1[i]-1][3]);
+      set_gpio_value(demux2_a2,chan[demux1[i]-1][2]);
+      set_gpio_value(demux2_a3,chan[demux1[i]-1][1]);
+      set_gpio_value(demux2_a4,chan[demux1[i]-1][0]);
+
+      //inner loop controls sampling
+      for(int j =0; j <= 29; j++){
+        set_gpio_value(mux_a0, chan[mux[i][j]-1][4]);
+        set_gpio_value(mux_a1, chan[mux[i][j]-1][3]);
+        set_gpio_value(mux_a2, chan[mux[i][j]-1][2]);
+        set_gpio_value(mux_a3, chan[mux[i][j]-1][1]);
+        set_gpio_value(mux_a4, chan[mux[i][j]-1][0]);
+
+        int volt_channel = 0;
+        int value = read_adc_raw(volt_channel);
+        int voltage = value*bits_to_volts;
+        printf(" %.3f",voltage);
+      }
+        printf("\n");
+        printf("--------------Current Configuration %d ------------------ \n",i+1);
+    }
+      k++;
+      printf(" ******************** Cylce %d *************************",k);
+  }
+  //setting all gpio mux pins to low
+  clean_up(demux1_a0);
+  clean_up(demux1_a1);
+  clean_up(demux1_a2);
+  clean_up(demux1_a3);
+  clean_up(demux1_a4);
+  clean_up(demux2_a0);
+  clean_up(demux2_a1);
+  clean_up(demux2_a2);
+  clean_up(demux2_a3);
+  clean_up(demux2_a4);
+  clean_up(mux_a0);
+  clean_up(mux_a1);
+  clean_up(mux_a2);
+  clean_up(mux_a3);
+  clean_up(mux_a4);
+
   return 0;
+}
+
+static int read_adc_raw(int channel){
+  char path[66];
+  char value_read[20];
+  int fd;
+  snprintf(path,66,"/sys/bus/iio/devices/iio:device1/in_voltage%d_raw",channel);
+  fd = open(path,O_WRONLY);
+  if (-1 == fd){
+    fprintf(stderr,"Failed to open adc for reading!\n");
+    return -1;
+  }
+  int fd_read = read(fd,value_read,20);
+  if (-1 == fd_read)) {
+    fprintf(stderr, "Failed to read value!\n");
+    return(-1);
+  }
+  close(fd);
+  return(atoi(value_read));
+
 }
 
 
@@ -127,4 +229,9 @@ static int read_gpio(int pin){
 	}
   close(fd);
   return(atoi(value_read));
+}
+
+static int clean_up(int pin){
+  set_gpio_value(pin, 0);
+  return 0;
 }
