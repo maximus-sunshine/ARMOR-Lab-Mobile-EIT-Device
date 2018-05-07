8ad3a6dedfad93b5295c53031ba546d8452fafb7
mrw008 <33675115+mrw008@users.noreply.github.com>
2018-05-07 16:05:57 -0700

    Update eit_sample.h

--- Scripts/eit_sample.h
+++ Scripts/eit_sample.h
@@ -1,84 +1,132 @@
-#ifndef eit_sample
-#define eit_sample
+/************************************************************** 
+* MAE 156B, Team 6
+*
+* Library header file for project specific functions (eit.c)
+*
+* 4/30/18
+***************************************************************/
 
-////DATA text file
-//#define VOLT_DATA_TXT "/home/debian/eit_sample/first_text.txt"
+#ifndef EIT_H
+#define EIT_H
 
-//TRUTH TABLE DIMENSIONS
-#define LOGIC_ROW 32
-#define LOGIC_COL 5
-
-#define CUR_ROW 20
-#define CUR_COL 10
-
-//MUX TRUTH TABLE DECLARATION
-//{A4,A3,A2,A1,A0}
-int CHAN[LOGIC_ROW][LOGIC_COL] = {{0,0,0,0,0},{0,0,0,0,1},{0,0,0,1,0},{0,0,0,1,1},
-                                 {0,0,1,0,0},{0,0,1,0,1},{0,0,1,1,0},{0,0,1,1,1},
-                                 {0,1,0,0,0},{0,1,0,0,1},{0,1,0,1,0},{0,1,0,1,1},
-                                 {0,1,1,0,0},{0,1,1,0,1},{0,1,1,1,0},{0,1,1,1,1},
-                                 {1,0,0,0,0},{1,0,0,0,1},{1,0,0,1,0},{1,0,0,1,1},
-                                 {1,0,1,0,0},{1,0,1,0,1},{1,0,1,1,0},{1,0,1,1,1},
-                                 {1,1,0,0,0},{1,1,0,0,1},{1,1,0,1,0},{1,1,0,1,1},
-                                 {1,1,1,0,0},{1,1,1,0,1},{1,1,1,1,0},{1,1,1,1,1}};
-
-//CURRENT LOGIC TABLE 
-//rows go from 100ma to 20000ma
-//columns {A4,A5,A6,A7,A8,A9,A10,A11,A12,A3}
-int CURRENT[CUR_ROW][CUR_COL] = {{0,0,1,0,0,0,0,0,0,0},{0,0,1,1,0,0,0,0,0,0},
-				{0,0,1,1,1,0,0,0,0,0},{0,0,1,1,1,1,0,0,0,0},
-				{0,0,1,1,1,1,1,0,0,0},{0,0,1,1,1,1,1,1,0,0},
-		                {1,0,1,1,1,1,1,1,0,0},{1,1,1,1,1,1,1,1,0,0},
-				{0,1,1,1,1,1,0,0,1,0},{1,1,1,1,1,1,0,0,1,0},
-		 		{0,1,1,1,1,1,1,0,0,1},{1,1,1,1,1,1,1,0,0,1},
-				{0,1,1,1,1,1,1,1,0,1},{1,1,1,1,1,1,1,1,0,1},
-		   		{0,1,1,1,1,0,1,0,1,1},{1,1,1,1,1,0,1,0,1,1},
-				{0,1,1,1,1,1,0,0,1,1},{1,1,1,1,1,1,0,0,1,1},
-				{0,1,1,1,1,1,1,0,1,1},{1,1,1,1,1,1,1,0,1,1}};
+/************************************************************** 
+* SAMPLE GEOMETRY (Must be square sample with N nodes per edge)
+***************************************************************/
 
 //NUMBER OF NODES
 #define NODAL_NUM 32
+
 //NUMBER OF NODES PER SIDE
-#define SIDE_LEN (NODAL_NUM/4)                                 
+#define SIDE_LEN (NODAL_NUM/4) 
 
-//FUNCTION DECLARATIONS
-int cur_gnd_config(int cur_mux[],int gnd_mux[]);
-int volt_samp_config(int cur_mux[],int gnd_mux[],int volt[][NODAL_NUM-2]);
-//int data_file_export(int volt_data);
+/************************************************************** 
+*ARRAY STRUCTURE FOR BUFFER
+***************************************************************/
 
-#define MAX_BUF 64	//max buffer length
+typedef struct {
+	
+	size_t used; //# of elements that have been put into array
+	size_t size; //size of array
+	int *array; // pointer array
+} Array; //name of struct
 
+/************************************************************** 
+*DATA FILE EXPORT PATH
+***************************************************************/
 
-///////////////PIN DECLARATIONS////////////
-//adc pins
-#define ADC_RESET_GPIO 13
-#define VOLT_CHANNEL 2    //adc channel to read voltage
-//current sense pin
-#define I_SENSE_RESET_GPIO 27
-//power mux declaration
-#define CUR_MUX_A0  8    //p8_35
-#define CUR_MUX_A1  9    //p8_33
-#define CUR_MUX_A2  10   //p8_31
-#define CUR_MUX_A3  87   //p8_29
-#define CUR_MUX_A4  86   //p8_27
-//ground mux declaraton
-#define GND_MUX_A0  70   //p8_45
-#define GND_MUX_A1  72   //p8_43
-#define GND_MUX_A2  74   //p8_41
-#define GND_MUX_A3  76   //p8_39
-#define GND_MUX_A4  78   //p8_37
-//voltage sampling delcaration
-#define VOLT_MUX_A0 15   //p9_24
-#define VOLT_MUX_A1 49   //p9_23
-#define VOLT_MUX_A2 14   //p9_26
-#define VOLT_MUX_A3 117  //p9_25
-#define VOLT_MUX_A4 115  //p9_27
+#define VOLT_DATA_TXT "/home/debian/sampling_text.txt"
 
-/////////////MUX ARRAY DECLARATIONS
-#define CURRENT_MUX_GPIO {CUR_MUX_A4,CUR_MUX_A3,CUR_MUX_A2,CUR_MUX_A1,CUR_MUX_A0}
-#define GROUND_MUX_GPIO {GND_MUX_A4,GND_MUX_A3,GND_MUX_A2,GND_MUX_A1,GND_MUX_A0}
-#define VOLTAGE_MUX_GPIO {VOLT_MUX_A4,VOLT_MUX_A3,VOLT_MUX_A2,VOLT_MUX_A1,VOLT_MUX_A0}
+/************************************************************************************
+* FUNCTION DECLARATIONS
+*************************************************************************************/
+
+/****************************************************************************
+* int cur_gnd_config(int cur[],int gnd[])
+*
+* Configures current and ground nodes in "across" switching pattern.
+* Works for square samples with N nodes per edge.
+*
+* Inputs :	cur[], zero current node array (size = N*4)
+*			gnd[], zero gnd node array     (size = N*4)
+* 
+* Outputs:	TODO: return -1 on failure
+*****************************************************************************/
+int cur_gnd_config(int cur[],int gnd[]);
 
 
+/****************************************************************************
+* int volt_samp_config(int cur[], int gnd[], int volt[][NODAL_NUM-2])
+*
+* Configures voltage sampling nodes in "across" switching pattern.
+* Works for square samples with N nodes per edge.
+* 
+* NOTE: must run cur_gnd_config first!
+*
+* Inputs :	cur[],	configured current node array	(size = [N*4])
+*			gnd[],	configured gnd node array		(size = [N*4])
+*			volt[],	zero voltage node array			(size = [N*4][N*4-2])		
+* 
+* Outputs:	TODO: -return -1 on failure
+*				  -add flag so this can't run unless cur_gnd_config has already been run
+*				  -alternatively, just merge this into cur_gnd_config...
+*****************************************************************************/
+int volt_samp_config(int cur[], int gnd[], int volt[][NODAL_NUM-2]);
 
-#endif	//eit_sample
+
+/****************************************************************************
+* int data_file_export(float volt_data[],size_t size)
+*
+* Exports voltage measurements to a text file path: VOLT_DATA_TXT
+* Writes at maximum 9 decimal places to text file
+*
+* Inputs :	volt_data[], float array of voltage measurements
+*			size, size_t data type describing the length of volt_data array
+* 
+* Outputs:	TODO: Add safety checks, add current sensing measurements to data file 
+*****************************************************************************/
+int data_file_export(float volt_data[],size_t size)
+
+	
+/****************************************************************************
+* void initArray(Array *a, size_t initialSize)
+*
+* Initializes a dynamic array in heap memory through malloc
+* Initializes used elements to zero and ininital array size according to intialSize
+*
+* Inputs :	Array *a, a pointer to stuct Array
+*			initialSize, initialze size of array
+* 
+* TODO: Add safety checks
+*****************************************************************************/
+void initArray(Array *a, size_t initialSize);
+	
+
+/****************************************************************************
+* void insertArray(Array *a, int element)
+*
+* Inserts elements into array and updates # of elements used in array
+* If the array fills up, it doubles in size through realloc
+*
+* Inputs :	Array *a, a pointer to stuct Array
+*			element, an integer that will be added to array
+* 
+* TODO: Add safety checks
+*****************************************************************************/
+void insertArray(Array *a, int element);
+
+/****************************************************************************
+* void sigint(int s __attribute__((unused)))
+*
+* Awaits a signal(ctrl c) and preforms cleanup duties before exiting program
+* Cleans up gpio pins and adc
+* Writes data to text file
+* 
+* Inputs :		ctrl c, user entered keyboard signal
+*			
+* 
+* TODO: Add safety checks,improve documentation, add additional clean up steps
+*****************************************************************************/
+void sigint(int s __attribute__((unused)));
+	
+
+#endif //EIT_H
