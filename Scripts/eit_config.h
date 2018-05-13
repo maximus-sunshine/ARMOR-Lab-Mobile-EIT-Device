6f9f4c8aad565041279a726319d2ac3677b08d34
mrw008 <33675115+mrw008@users.noreply.github.com>
2018-05-12 18:39:43 -0700

    Update eit_config.h

--- Scripts/eit_config.h
+++ Scripts/eit_config.h
@@ -30,31 +30,48 @@
 ***************************************************************/
 
 //ADC PINS
-#define ADC_RESET_GPIO 13	//p9_19
+#define ADC_RESET_GPIO   15	//p9_24
 #define VOLT_CHANNEL 2		//adc channel to read voltage
 
 //CURRENT SENSE PINS
 #define I_SENSE_RESET_GPIO 27	//p8_17
 
+//CURRENT LOGIC PINS
+#define cur_source_A4  35   //p8_6
+#define cur_source_A5  66   //p8_7
+#define cur_source_A6  68   //p8_10
+#define cur_source_A7  34   //p8_5
+#define cur_source_A8  38   //p8_3
+#define cur_source_A9  69   //p8_9
+#define cur_source_A10 45   //p8_11
+#define cur_source_A11 39   //p8_4
+#define cur_source_A12 67   //p8_8
+#define cur_source_A13 44   //p8_12
+
+//mux enable pin
+#define cur_mux_enable  79    //p8_38
+#define gnd_mux_enable  71    //p8_46
+#define volt_mux_enable 62    //p8_21
+
 //MUX LOGIC PINS
 //power mux declaration
-#define CUR_MUX_A0  8	//p8_35
-#define CUR_MUX_A1  9	//p8_33
-#define CUR_MUX_A2  10	//p8_31
-#define CUR_MUX_A3  87	//p8_29
-#define CUR_MUX_A4  86	//p8_27
+#define CUR_MUX_A0  8	 //p8_35
+#define CUR_MUX_A1  9	 //p8_33
+#define CUR_MUX_A2  10	 //p8_31
+#define CUR_MUX_A3  87	 //p8_29
+#define CUR_MUX_A4  86	 //p8_27
 //ground mux declaraton
-#define GND_MUX_A0  70	//p8_45
-#define GND_MUX_A1  72	//p8_43
-#define GND_MUX_A2  74	//p8_41
-#define GND_MUX_A3  76	//p8_39
-#define GND_MUX_A4  78	//p8_37
+#define GND_MUX_A0  70	 //p8_45
+#define GND_MUX_A1  72	 //p8_43
+#define GND_MUX_A2  74	 //p8_41
+#define GND_MUX_A3  76	 //p8_39
+#define GND_MUX_A4  78	 //p8_37
 //voltage sampling delcaration
-#define VOLT_MUX_A0 15	//p9_24
-#define VOLT_MUX_A1 49	//p9_23
-#define VOLT_MUX_A2 14	//p9_26
-#define VOLT_MUX_A3 117	//p9_25
-#define VOLT_MUX_A4 115	//p9_27
+#define VOLT_MUX_A0  7	 //p9_42
+#define VOLT_MUX_A1  20	 //p9_41
+#define VOLT_MUX_A2  14	 //p9_26
+#define VOLT_MUX_A3  117 //p9_25
+#define VOLT_MUX_A4  115 //p9_27
 //array definitions
 #define CURRENT_MUX_GPIO	{CUR_MUX_A4, CUR_MUX_A3, CUR_MUX_A2, CUR_MUX_A1, CUR_MUX_A0}
 #define GROUND_MUX_GPIO		{GND_MUX_A4, GND_MUX_A3, GND_MUX_A2, GND_MUX_A1, GND_MUX_A0}
@@ -86,15 +103,14 @@ int CHAN[LOGIC_ROW][LOGIC_COL] = {{0,0,0,0,0},{0,0,0,0,1},{0,0,0,1,0},{0,0,0,1,1
 //CURRENT SOURCE SWITCHING TRUTH TABLE
 //rows:		100ua to 20000ua in 100ua steps
 //columns:	logic pins {A4,A5,A6,A7,A8,A9,A10,A11,A12,A13} 
-int CURRENT[CUR_ROW][CUR_COL] = {{0,0,1,0,0,0,0,0,0,0},{0,0,1,1,0,0,0,0,0,0},
-								{0,0,1,1,1,0,0,0,0,0},{0,0,1,1,1,1,0,0,0,0},
-								{0,0,1,1,1,1,1,0,0,0},{0,0,1,1,1,1,1,1,0,0},
-								{1,0,1,1,1,1,1,1,0,0},{1,1,1,1,1,1,1,1,0,0},
-								{0,1,1,1,1,1,0,0,1,0},{1,1,1,1,1,1,0,0,1,0},
-								{0,1,1,1,1,1,1,0,0,1},{1,1,1,1,1,1,1,0,0,1},
-								{0,1,1,1,1,1,1,1,0,1},{1,1,1,1,1,1,1,1,0,1},
-								{0,1,1,1,1,0,1,0,1,1},{1,1,1,1,1,0,1,0,1,1},
-								{0,1,1,1,1,1,0,0,1,1},{1,1,1,1,1,1,0,0,1,1},
-								{0,1,1,1,1,1,1,0,1,1},{1,1,1,1,1,1,1,0,1,1}};
-
+int CURRENT[CUR_ROW][CUR_COL] = {{1,0,0,0,0,0,0,0,0,0},{1,1,0,0,0,0,0,0,0,0},
+								 {1,1,1,0,0,0,0,0,0,0},{1,1,1,1,0,0,0,0,0,0},
+								 {1,1,1,1,1,0,0,0,0,0},{1,1,1,1,1,1,0,0,0,0},
+								 {1,1,1,1,1,1,1,0,0,0},{1,1,1,1,1,1,1,1,0,0},
+								 {1,1,1,1,1,0,0,0,1,0},{1,1,1,1,1,1,0,0,1,0},
+								 {1,1,1,1,1,1,1,0,1,0},{1,1,1,1,1,1,1,1,1,0},
+								 {1,1,1,0,0,0,1,0,1,1},{1,1,1,0,0,0,1,1,1,1},
+								 {1,1,1,0,1,0,1,0,1,1},{1,1,1,1,0,0,0,0,1,1},
+								 {1,1,1,1,0,0,1,0,1,1},{1,1,1,1,1,0,0,0,1,1},
+								 {1,1,1,1,1,0,1,0,1,1},{1,1,1,1,1,1,0,0,1,1}};
 #endif //EIT_CONFIG_H
