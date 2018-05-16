7bcd95abdb9854a519cbd15d7244e88cebb2bc10
Demo User <debian@beaglebone.localdomain>
2018-05-16 05:24:09 +0000

    Added OLED libraries... not working right now though..

---
+++ OLED/example_app.h
@@ -0,0 +1,21 @@
+#ifndef EXAMPLE_APP_H
+#define EXAMPLE_APP_H
+
+void testdrawline();
+void testdrawrect();
+void testfillrect();
+void testdrawcircle();
+void testdrawroundrect();
+void testdrawtriangle();
+void testfilltriangle();
+void testdrawchar();
+void testscrolltext();
+void display_texts();
+void display_texts();
+void display_bitmap();
+void display_invert_normal();
+void testdrawbitmap(const unsigned char *bitmap, unsigned char w, unsigned char h);
+void testdrawbitmap_eg();
+void deeplyembedded_credits();
+
+#endif
