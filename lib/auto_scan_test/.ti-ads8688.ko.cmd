27d0a96705546052aadcadbd38ac4380fdabb5c2
maximus-sunshine <mwsun@ucsd.ed>
2018-06-08 12:26:09 -0700

    Purged unnecessary files. Repository cleaned up and ready for delivery.
    For old code, branch a old version of github repo.

--- lib/auto_scan_test/.ti-ads8688.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/debian/MAE156B_Team6/lib/auto_scan_test/ti-ads8688.ko := ld -EL -r  -T ./scripts/module-common.lds --build-id  -T ./arch/arm/kernel/module.lds -o /home/debian/MAE156B_Team6/lib/auto_scan_test/ti-ads8688.ko /home/debian/MAE156B_Team6/lib/auto_scan_test/ti-ads8688.o /home/debian/MAE156B_Team6/lib/auto_scan_test/ti-ads8688.mod.o ;  true
--- lib/auto_scan_test/.tmp_versions/ti-ads8688.mod
+++
@@ -1,3 +0,0 @@
-/home/debian/MAE156B_Team6/lib/auto_scan_test/ti-ads8688.ko
-/home/debian/MAE156B_Team6/lib/auto_scan_test/ti-ads8688.o
-
